c8396f3d66456b03ab745b149e7df504bab4c51c
Rafael Nogueira <rafanog.linux@gmail.com>
2019-11-20 20:26:34 -0300

    Primeiro commit projeto InForma

---
+++ InForma/InForma/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InForma.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
