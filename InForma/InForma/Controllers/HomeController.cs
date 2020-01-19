2f2ba5883fd02e09b029792f31af440189e94292
Rafael Nogueira <rafanog.linux@gmail.com>
2020-01-19 12:19:35 -0300

    Commit parte do front

--- InForma/InForma/Controllers/HomeController.cs
+++ InForma/InForma/Controllers/HomeController.cs
@@ -28,6 +28,16 @@ namespace InForma.Controllers
             return View();
         }
 
+        public IActionResult Unidade()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
